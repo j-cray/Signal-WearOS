e074dca92ba9cd21193981fdd6ea3141885e1aa5
Jake Wray <jakewray@mailbox.org>
2026-01-10 09:23:11 -0800

    flake: Add glibc.dev and set BINDGEN_EXTRA_CLANG_ARGS
    This ensures that bindgen can locate the necessary C headers.

--- flake.nix
+++ flake.nix
@@ -53,7 +53,10 @@
           cmake
           gradle
           protobuf
-          llvmPackages.libclang # Added libclang
+          llvmPackages.libclang
+
+          # C Headers
+          glibc.dev
 
           # Android
           android-sdk
@@ -70,6 +73,9 @@
 
           # Set LIBCLANG_PATH for bindgen
           export LIBCLANG_PATH="${pkgs.llvmPackages.libclang.lib}/lib"
+
+          # Set BINDGEN_EXTRA_CLANG_ARGS to find C headers
+          export BINDGEN_EXTRA_CLANG_ARGS="-I${pkgs.glibc.dev}/include -I${pkgs.llvmPackages.libclang.lib}/lib/clang/${pkgs.llvmPackages.libclang.version}/include"
 
           unset ANDROID_SDK_ROOT
 
